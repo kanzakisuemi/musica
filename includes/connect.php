6262f098360b486a2c137687ab004d771d5b8ae3
kanzakisuemi <jsuemikanzaki@gmail.com>
2023-02-09 18:12:50 -0300

    new commit

--- includes/connect.php
+++ includes/connect.php
@@ -1,18 +1,17 @@
-
 <?php
-    header("location:alert.php");
-    include 'open_dbconn.php';
-    $sql = "INSERT INTO aboutmusic (song_name, artist_name, recommendation, users_name, users_age) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssi", $song_name, $artist_name, $recommendation, $users_name, $users_age);
+  header("location:alert.php");
+  include 'open_dbconn.php';
+  $sql = "INSERT INTO aboutmusic (song_name, artist_name, recommendation, users_name, users_age) VALUES (?, ?, ?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ssssi", $song_name, $artist_name, $recommendation, $users_name, $users_age);
 
-    $song_name = $_POST["song_name"];
-    $artist_name = $_POST["artist_name"];
-    $recommendation = $_POST["recommendation"];
-    $users_name = $_POST["users_name"];
-    $users_age = $_POST["users_age"]; 
+  $song_name = $_POST["song_name"];
+  $artist_name = $_POST["artist_name"];
+  $recommendation = $_POST["recommendation"];
+  $users_name = $_POST["users_name"];
+  $users_age = $_POST["users_age"]; 
 
-    $stmt->execute();
-    $stmt->close();
-    include 'close_dbconn.php';
+  $stmt->execute();
+  $stmt->close();
+  include 'close_dbconn.php';
 ?>
